2e88b6bd16522b9c9f5da80bc9dd33314a212805
Saveliy <sivilijbrindak@gmail.com>
2026-02-07 10:47:02 +0200

    fff

---
+++ js/components.js
@@ -0,0 +1,5 @@
+async function loadComponent(name_file, name_sel){
+    let component = await fetch(name_file)
+    let div_cont = document.querySelector(name_sel)
+    div_cont.innerHTML = await component.text()
+}
