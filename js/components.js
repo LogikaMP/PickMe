504bfad1574691a64baff7fb723913ec4b5835cd
Saveliy <sivilijbrindak@gmail.com>
2026-02-08 12:27:16 +0200

    jjj

--- js/components.js
+++ js/components.js
@@ -3,3 +3,18 @@ async function loadComponent(name_file, name_sel){
     let div_cont = document.querySelector(name_sel)
     div_cont.innerHTML = await component.text()
 }
+
+async function loadData(name_file){
+    fetch(name_file).then(function(data){
+        return data
+    }).then(function(data2){
+        return data2.json()
+    })
+}
+
+function choice(arr) {
+  if (!arr.length) {
+    throw new Error("Cannot choose from an empty array");
+  }
+  return arr[Math.floor(Math.random() * arr.length)];
+}
--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(){
     loadComponent('../components/header.html', '.header')
+
+
+    let all_product
+    loadData('../data/all_product.json').then(function(obg){
+        all_product = obg
+    })
+    console.log(all_product)
 })
+
+// 6 6 - https://codepen.io/Forte_high/pen/RNRNzPQ 
