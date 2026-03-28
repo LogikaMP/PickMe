5bbdca52b7f6dba8545bc45b3831922335649177
LogikaMP <logika690@gmail.com>
2026-03-28 11:33:19 +0200

    jj

--- js/components.js
+++ js/components.js
@@ -1,7 +1,7 @@
 async function loadComponent(name_file, name_sel){
     let component = await fetch(name_file)
     let div_cont = document.querySelector(name_sel)
-    div_cont.innerHTML = await component.text()
+    div_cont.innerHTML += await component.text()
 }
 
 async function loadData(name_file){
@@ -15,4 +15,35 @@ function choice(arr) {
     throw new Error("Cannot choose from an empty array");
   }
   return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function opencart(){
+  if(document.querySelector(".cart")){
+    return
+  }
+  loadComponent("../component/cart.html","main")
+}
+
+function clouscart() {
+  let cart = document.querySelector(".cart")
+  let main = document.querySelector("main")
+  main.removeChild(cart)
+}
+
+function addtocart(product) {
+  let cart = localStorage.getItem("cart")
+  cart = JSON.parse(cart)
+  if(cart) {
+
+
+  }
+  else{
+    cart =[]
+    let item = {"id":product["id"], 
+                "name": product["name"],
+              "price": product["price"],
+              "count": product("count")}
+    cart.push(item)
+    cart = JSON.s
+  }
 }
